67943aa2bcde19613fbd89d0addbea89cb1ce25b
Diego Sá de Lima <76665875+DiegoSadeLima@users.noreply.github.com>
2021-05-03 11:02:15 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -50,7 +50,7 @@ def predict():
 if __name__ == '__main__':
     #start flask
     port = os.environ.get('PORT','5000')
-    app.run(host='127.0.0.1', port=port) 
+    app.run(host='0.0.0.0', port=port) 
 
 
 # In[ ]:
